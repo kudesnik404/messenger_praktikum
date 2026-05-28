daae2e4c08317829839fa5cef06ff8b2efcd019f
Victoria Demidenko <demidenko@adsfin.pro>
2026-05-28 05:11:07 +0300

    ESLint fix

--- eslint.config.js
+++ eslint.config.js
@@ -4,4 +4,18 @@ import tseslint from 'typescript-eslint';
 export default [
     js.configs.recommended,
     ...tseslint.configs.recommended,
+    {
+        ignores: [
+            "**/node_modules/**",
+            "**/dist/**",
+            "**/build/**",
+            "**/*.config.*",
+            "**/.stylelintrc.cjs"
+        ]
+    },
+    {
+        rules: {
+            "eol-last": ["error", "always"]
+        }
+    }
 ];
